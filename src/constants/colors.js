ed40bcc34cb165baa5e3cd40aefb2bdca543bf06
An Hee Jae <gmlwo000810@gmail.com>
2025-02-12 13:32:52 +0900

    no message

---
+++ src/constants/colors.js
@@ -0,0 +1,29 @@
+export const MAIN = {
+  BLUE: '#265cff',
+  GREEN: '#baff29',
+}
+
+export const SECONDARY = {
+  BLUE: '#b1d6ff',
+  GREEN: '#edfea1',
+}
+
+export const GRAY_SCALE = {
+  GRAY100: '#E4E7F0',
+  GRAY300: '#D3D8E5',
+  GRAY500: '#73798D',
+  GRAY700: '#535761',
+}
+
+export const BACKGROUND = {
+  WHITE: '#F8F8FF'
+}
+
+export const TEXT = {
+  BLACK: '#1a1b1d'
+}
+
+export const WARNING = {
+  TEXT: '#E73E3E',
+  BACKGROUND: '#FFEDED',
+}
