f90f1bd77b4844c5d8b553baec285f39b1f28278
An Hee Jae <gmlwo000810@gmail.com>
2025-03-20 17:56:53 +0900

    로그인 상태 추가

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,42 @@ import MainPage from "./pages/MainPage";
 import StudioPage from "./pages/StudioPage";
 import LoginPage from "./pages/LoginPage"
 import SearchResultPage from "./pages/SearchResultPage";
+import MyPage from "./pages/MyPage";
+import ExchangePage from "./pages/ExchangePage";
+import { useState, useEffect } from "react";
+
 
 function App() {
+
+  //로그인 상태
+  const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem("token"));
+
+  // 로그인 상태 변경 함수
+  const updateLoginStatus = (status) => {
+    setIsLoggedIn(status);
+  };
+
+  useEffect(() => {
+    const handleStorageChange = () => {
+      setIsLoggedIn(!!localStorage.getItem("token"));
+    };
+
+    window.addEventListener("storage", handleStorageChange);
+    return () => window.removeEventListener("storage", handleStorageChange);
+  }, []);
+
   return (
     <>
       <AppWrapper>
-        <FixedNavBar />
+        <FixedNavBar isLoggedIn={isLoggedIn} updateLoginStatus={updateLoginStatus} />
         <Routes>
           <Route path="/" element={<MainPage />} />
-          <Route path="/login" element={<LoginPage/>} />
+          <Route path="/login" element={<LoginPage updateLoginStatus={updateLoginStatus}/>} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/video-studio" element={<StudioPage />} />
           <Route path="/search" element={<SearchResultPage/>} />
+          <Route path="/mypage" element={<MyPage/>} />
+          <Route path="/exchange" element={<ExchangePage/>} />
         </Routes>
       </AppWrapper>
     </>
@@ -28,14 +52,15 @@ function App() {
 const AppWrapper = styled.div`
 display: flex;
 flex-direction: column;
-min-height: 100vh;
-`;
+min-height: 100vh;`
+;
 
 const FixedNavBar = styled(NavBar)`
   position: fixed;
   top: 0; 
   left: 0;
   z-index: 1000; 
-`
+  `
+
 
 export default App;
