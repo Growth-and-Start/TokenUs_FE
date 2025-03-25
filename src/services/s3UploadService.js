875e06423d3dd6949b0a5d961f3a0eb7891679cf
An Hee Jae <gmlwo000810@gmail.com>
2025-03-25 17:49:38 +0900

    이미지 업로드 API 연동 파일 이름 수정

--- src/services/s3UploadService.js
+++ src/services/s3UploadService.js
@@ -3,13 +3,16 @@ import axios from "axios";
 const API_URL = "http://54.180.83.169:8080/s3";
 
 //presigned url 요청
-export const getPresignedUrl = async (fileName, fileType) => {
-  const response = await axios.post(`${API_URL}/presigned-url`, {
-    fileName,
-    fileType,
+export const getPresignedUrl = async (fileName, contentType) => {
+  const response = await axios.get(`${API_URL}/presigned-url`, {
+    params: {
+      fileName,
+      contentType,
+    },
   });
   return response.data;
 };
+
 
 
 //Presigned URL을 사용하여 S3에 이미지 업로드
@@ -18,3 +21,4 @@ export const uploadImageToS3 = async (uploadURL, file) => {
     headers: { "Content-Type": file.type },
   });
 };
+
