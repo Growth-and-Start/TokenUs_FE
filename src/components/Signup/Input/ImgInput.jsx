6cb48150618ebe7039483f92fa1d888d49eb67b4
An Hee Jae <gmlwo000810@gmail.com>
2025-03-30 16:53:45 +0900

    회원가입 시 프로필 사진 선택 버튼 컴포넌트 스타일링 변경

--- src/components/Signup/Input/ImgInput.jsx
+++ src/components/Signup/Input/ImgInput.jsx
@@ -1,30 +1,44 @@
-
-
 import styled from "styled-components";
+import { GRAY_SCALE, MAIN } from "../../../constants/colors";
 
 function ImgInput({ name, onChange, data, children }) {
   return (
     <Wrapper>
-      <Left>
-        <Label htmlFor={name}>{children}</Label>
-        <Input
-          id={name}
-          name={name}
-          type="file"
-          accept="image/*"
-          onChange={onChange}
-        />
-      </Left>
-      {data && <Preview src={URL.createObjectURL(data)} alt="preview" />}
+      <Title>{children}</Title>
+      <Input>
+        <Left>
+          <StyledLabel htmlFor={name}>파일 선택</StyledLabel>
+          <HiddenInput
+            id={name}
+            name={name}
+            type="file"
+            accept="image/*"
+            onChange={onChange}
+          />
+        </Left>
+        <FileName>{data && data.name}</FileName>
+        {data && <Preview src={URL.createObjectURL(data)} alt="preview" />}
+      </Input>
     </Wrapper>
   );
 }
 
 const Wrapper = styled.div`
   display: flex;
+  flex-direction: column;
+`;
+
+const Title = styled.div`
+  font-weight: 520;
+  font-size: 15px;
+  padding-bottom: 10px;
+`;
+
+const Input = styled.div`
+  display: flex;
   flex-direction: row;
-  gap: 16px;
   align-items: center;
+  justify-content: space-between;
 `;
 
 const Left = styled.div`
@@ -33,13 +47,34 @@ const Left = styled.div`
   gap: 8px;
 `;
 
-const Label = styled.label`
-  font-weight: 520;
-  font-size: 15px;
-  padding-bottom: 5px;
+const StyledLabel = styled.label`
+  width: 100px;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #f2f5fe;
+  color: ${MAIN.BLUE};
+  border: 1.5px solid ${GRAY_SCALE.GRAY300};
+  border-radius: 10px;
+  font-size: 16px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #eaf0ff;
+  }
 `;
 
-const Input = styled.input``;
+const HiddenInput = styled.input`
+  display: none;
+`;
+
+const FileName = styled.div`
+color: ${GRAY_SCALE.GRAY500};
+font-size: 12px;
+max-width: 145px;
+word-wrap: break-word;
+`;
 
 const Preview = styled.img`
   width: 80px;
