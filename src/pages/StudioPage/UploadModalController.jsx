ca87205dde8ffc7d2532ef1bff6f5ce6a79a562d
An Hee Jae <gmlwo000810@gmail.com>
2025-03-30 16:56:05 +0900

    비디오 정보 POST 기능 완성

--- src/pages/StudioPage/UploadModalController.jsx
+++ src/pages/StudioPage/UploadModalController.jsx
@@ -1,32 +1,35 @@
 import { useEffect, useState } from "react";
 import UploadVideoModal1 from "../../components/Modal/UploadVideo/UploadVideoModal1";
 import UploadVideoModal2 from "../../components/Modal/UploadVideo/UploadVideoModal2";
+import { uploadContent } from "../../utils/upload";
+import { postVideoData } from "../../services/videoService";
 
 function UploadModalController({ onClose }) {
 
   //사용자 입력 데이터 - 비디오 정보
   const [videoData, setVideoData] = useState({
-    title: "",
-    detail: "",
-    fileUrl: "",
+    videoTitle: "",
+    videoDetail: "",
+    videoUrl: "",
     isOpen: true,
     thumbnailUrl: "",
   });
 
-    //사용자 입력 데이터 - NFT 정보
-    const [nftData, setNftData] = useState({
-      metadataUri: "",
-      totalSupply: "1",
-      nftName: "",
-      nftSymbol: "",
-    });
-
+  //사용자 입력 데이터 - NFT 정보
+  const [nftData, setNftData] = useState({
+    metadataUri: "",
+    totalSupply: "1",
+    nftName: "",
+    nftSymbol: "",
+    nftPrice: "0.001",
+  });
 
   //비디오 업르드 단계(1: 기본 정보 및 유사도 검사, 2: NFT 발행)
   const [step, setStep] = useState(1);
 
   //다음 페이지로 넘어가기
   const handleNext = () => {
+    handleSubmitVideoInfo();
     setStep((prev) => prev + 1);
   };
 
@@ -38,42 +41,65 @@ function UploadModalController({ onClose }) {
   //사용자 입력 데이터(비디오 정보) 변경
   const handleChangeVideoInfo = (e) => {
     const { name, type, value, files } = e.target;
-  
+
     let finalValue = value;
-  
+
     if (type === "file") {
       finalValue = files[0];
     }
-  
+
     if (name === "isOpen") {
       finalValue = value === "true";
     }
-  
+
     setVideoData((prevData) => ({
       ...prevData,
       [name]: finalValue,
     }));
-
   };
-  
 
   //사용자 입력 데이터(NFT 정보) 변경
   const handleChangeNFTInfo = (e) => {
-    const {name, type, value, files} = e.target;
+    const { name, type, value, files } = e.target;
     setNftData((prevData) => ({
       ...prevData,
       [name]: type === "file" ? files[0] : value,
     }));
+  };
+
+  const removeVideo = () => {
+    setVideoData((prevData) => ({
+      ...prevData,
+      "videoUrl": null,
+    }));
   }
 
-  //비디오 업로드 완료
-  const handleSubmit = () =>{
+  // 비디오 정보 POST
+  const handleSubmitVideoInfo = async () => {
+    try{
+      const uploadedVideoUrl = await uploadContent(videoData.videoUrl, "video");
+      const uploadedImageUrl = await uploadContent(videoData.thumbnailUrl, "thumbnail"); 
+      const finalVideoData = {...videoData, videoUrl: uploadedVideoUrl, thumbnailUrl: uploadedImageUrl};
+      
+
+      postVideoData(finalVideoData);
+
+       //temp for test
+       console.log("최종 전송 데이터", finalVideoData);
+    }catch(error){
+      console.log("비디오 POST 실패: ", error.response?.data?.message || "서버 오류" )
+    }
+
+  };
+
+  // NFT 정보 POST(비디오 업로드 완료)
+  const handleSubmitNFTInfo = () => {
     //백엔드 연동 코드 - 비디오 정보/NFT 정보 전달
 
-    onClose();  //모달 창 닫기
-  }
+    onClose(); //모달 창 닫기
+  };
 
-//temp for test
+  //temp for test
   useEffect(() => {
     console.log("videoData 변경:", videoData);
   }, [videoData]);
@@ -81,17 +107,22 @@ function UploadModalController({ onClose }) {
   return (
     <>
       {step === 1 && (
-        <UploadVideoModal1 onCancel={onClose} onNext={handleNext} onChange={handleChangeVideoInfo} data={videoData}/>
+        <UploadVideoModal1
+          onCancel={onClose}
+          onNext={handleNext}
+          onChange={handleChangeVideoInfo}
+          data={videoData}
+          onRemove={removeVideo}
+        />
       )}
       {step === 2 && (
         <UploadVideoModal2
-        onBack={handleBack}
-        onSubmit={handleSubmit}
-        onChange={handleChangeNFTInfo}
-        data={nftData}
-        setNftData={setNftData}
-      />
-      
+          onBack={handleBack}
+          onSubmit={handleSubmitNFTInfo}
+          onChange={handleChangeNFTInfo}
+          data={nftData}
+          setNftData={setNftData}
+        />
       )}
     </>
   );
