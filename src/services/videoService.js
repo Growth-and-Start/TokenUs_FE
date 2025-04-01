29ab249883654c6d99a8eda9b8de35e267c390a7
An Hee Jae <gmlwo000810@gmail.com>
2025-04-01 15:19:08 +0900

    영상 유사도 검사 요청 및 비디오 목록 요청 API 함수 추가

--- src/services/videoService.js
+++ src/services/videoService.js
@@ -20,4 +20,46 @@ export const postVideoData = async (data) => {
 
 
 //영상 유사도 검사 결과 요청
+export const checkVideoSimilarity = async (videoUrl) => {
 
+  //temp for test
+  console.log("유사도 검사 요청 비디오 URL: ", videoUrl);
+
+  const token = localStorage.getItem("accessToken");
+  //temp for test
+  console.log("Access Token: ", token);
+  if (!token) {
+    console.log("로그인이 필요합니다.");
+    return;
+  }
+
+  return await axios.post(`${API_URL}/similarity_check`, { videoUrl }, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+//비디오 목록 요청
+export const getVideoList = async (isSubscribe) => {
+
+  const token = localStorage.getItem("accessToken");
+
+  if (!token) {
+    console.log("로그인이 필요합니다.");
+    return;
+  }
+
+  const response = await axios.get(`${API_URL}/get_opened_videos`, {
+    params: { isSubscribe },
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  //temp for test
+  console.log(response);
+
+  return response.data.result;
+};
