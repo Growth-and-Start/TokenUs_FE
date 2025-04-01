78e0ec10a748f69c9c427baee68118067ba81eda
An Hee Jae <gmlwo000810@gmail.com>
2025-04-01 15:35:52 +0900

    썸네일 prop 추가 및 날짜 데이터 포맷 변경

--- src/components/VideoContent/VideoCard,.jsx
+++ src/components/VideoContent/VideoCard,.jsx
@@ -1,44 +1,46 @@
 import styled from "styled-components";
 
-function VideoCard({title, channel, date}) {
-  return(
-    <>
+function VideoCard({ title, channel, date, thumbnailUrl }) {
+  const [year, month, day] = date.split("T")[0].split("-");
+  const formattedDate = `${year} / ${month} / ${day}`;
+  return (
     <VideoCardWrapper>
-      <Thumbnail/>
+      <Thumbnail thumbnailUrl={thumbnailUrl} />
       <Title>{title}</Title>
       <Channel>{channel}</Channel>
-      <Date>{date}</Date>
+      <Date>{formattedDate}</Date>
     </VideoCardWrapper>
-    </>
-  )
+  );
 }
 
 const VideoCardWrapper = styled.div`
   display: flex;
   flex-direction: column;
   box-sizing: border-box;
-  width:250px;
-  gap:2px;
+  gap: 2px;
   padding-bottom: 20px;
-`
+`;
 
 const Thumbnail = styled.div`
-background-color: gray;
-border-radius: 5px;
-width: 100%;
-height: 140px;
-`
+  background-color: gray;
+  background-image: url(${(props) => props.thumbnailUrl});
+  background-size: cover;
+  background-position: center;
+  border-radius: 5px;
+  width: 240px;
+  height: 135px;
+`;
 
 const Title = styled.div`
-font-size: 15px;
-`
+  font-size: 15px;
+`;
 
 const Channel = styled.div`
-font-size: 12px;
-`
+  font-size: 12px;
+`;
 
 const Date = styled.div`
-font-size: 12px
-`
+  font-size: 12px;
+`;
 
 export default VideoCard;
