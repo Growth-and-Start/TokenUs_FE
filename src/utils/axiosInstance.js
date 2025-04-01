54eb09558f4e8931f88bec33c43871da2b7260ab
An Hee Jae <gmlwo000810@gmail.com>
2025-04-01 16:14:48 +0900

    axios 인터셉터 util 함수

---
+++ src/utils/axiosInstance.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+import { refreshAccessToken } from "../services/authService";
+
+const axiosInstance = axios.create({
+  baseURL: "http://54.180.83.169:8080",
+  withCredentials: true,
+});
+
+// 요청 시 accessToken 자동 추가
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("accessToken");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+// 응답 시 accessToken 만료 감지 -> 갱신 후 재시도
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        const newToken = await refreshAccessToken();
+        originalRequest.headers.Authorization = `Bearer ${newToken}`;
+        return axiosInstance(originalRequest); 
+      } catch (refreshError) {
+        console.log("리프레시 토큰도 만료됨");
+        window.location.href = "/login"; // 로그인 페이지로 이동
+        return Promise.reject(refreshError);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
