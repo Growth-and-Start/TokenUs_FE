2a76d0b2bf70da6e62512602dd4893c68b7385f0
An Hee Jae <gmlwo000810@gmail.com>
2025-04-02 12:13:11 +0900

    검색 결과 요청(영상 목록) 서비스 함수 추가

--- src/services/videoService.js
+++ src/services/videoService.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import axiosInstance from "../utils/axiosInstance";
 
 const API_URL = "http://54.180.83.169:8080/video";
@@ -29,6 +30,15 @@ export const getVideoList = async (isSubscribe) => {
     params: { isSubscribe },
   });
 
-  console.log(response);
   return response.data.result;
 };
+
+
+//검색 결과 요청 (비디오 목록)
+export const getSearchResult = async (searchFor) => {
+  const response = await axios.get(`${API_URL}/search`, {
+    params: {searchFor},
+  });
+
+  return response.data.result;
+}
