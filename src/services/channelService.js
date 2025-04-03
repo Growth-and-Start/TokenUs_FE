c8c2cad30c46c9fd00c688d7b8b1e7ca662db75f
An Hee Jae <gmlwo000810@gmail.com>
2025-04-03 17:50:43 +0900

    구독 취소 API 요청 수정 (파라미터 전달 방식)

--- src/services/channelService.js
+++ src/services/channelService.js
@@ -5,7 +5,7 @@ const API_URL = "http://54.180.83.169:8080/user";
 
 //검색 결과 요청 (채널 목록)
 export const getSearchResult_channel = async (searchFor) => {
-  const response = await axios.get(`${API_URL}/search`, {
+  const response = await axiosInstance.get(`${API_URL}/search`, {
     params: { searchFor },
   });
 
@@ -22,6 +22,14 @@ export const postSubscribe = async (targetId) => {
 
 
 //채널 구독 취소 요청
+// export const deleteSubscribe = async (targetId) => {
+//   return await axiosInstance.delete(`${API_URL}/unsubscribe/${targetId}`);
+// };
+
 export const deleteSubscribe = async (targetId) => {
-  return await axiosInstance.delete(`${API_URL}/unsubscribe/${targetId}`);
+  return await axiosInstance.delete(`${API_URL}/unsubscribe`, {
+    params: { targetId },
+  }
+
+  );
 };
