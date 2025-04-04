f00b5480e44a4487c9b82c2663187947908a0082
An Hee Jae <gmlwo000810@gmail.com>
2025-04-04 12:15:08 +0900

    비디오 시청 페이지 생성

--- src/pages/WatchVideoPage/index.jsx
+++ src/pages/WatchVideoPage/index.jsx
@@ -3,31 +3,46 @@ import styled from "styled-components";
 
 //비디오 시청 페이지
 function WatchVideoPage() {
-  const{videoTitle} = useParams();
+  const { videoTitle } = useParams();
   const location = useLocation();
-  const {title, channel, formattedDate, videoUrl} = location.state || {};
-
+  const { title, channel, formattedDate, videoUrl } = location.state || {};
 
   return (
     <>
       <Wrapper>
-        <StyledVideo controls width="60%" src={videoUrl} />
+        <ContentArea>
+          <StyledVideo controls width="60%" src={videoUrl} />
+          <VideoInfo></VideoInfo>
+        </ContentArea>
+        <NFTArea></NFTArea>
+
         <h2>{title}</h2>
-        <p>{channel} | {formattedDate}</p>
+        <p>
+          {channel} | {formattedDate}
+        </p>
       </Wrapper>
     </>
   );
 }
 
 const Wrapper = styled.div`
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
+const ContentArea = styled.div``;
+
+const NFTArea = styled.div``;
+
 const StyledVideo = styled.video`
-border-radius: 10px;
+  border-radius: 10px;
 `;
+
+const VideoInfo = styled.div``;
+
+
+
 
 export default WatchVideoPage;
