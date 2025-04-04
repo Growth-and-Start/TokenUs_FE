96e6c0a393cc9f7b9d98bcb1775df2bfc1bbf46e
An Hee Jae <gmlwo000810@gmail.com>
2025-04-04 16:13:20 +0900

    비디오 상세 설명란 컴포넌트

---
+++ src/components/VideoContent/VideoDescription.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import { GRAY_SCALE, TEXT } from "../../constants/colors";
+
+function VideoDescription ({children}) {
+  return (
+    <>
+    <Description>
+      {children}
+    </Description>
+    </>
+  )
+}
+
+const Description = styled.div`
+border-radius: 10px;
+border: 1px solid ${GRAY_SCALE.GRAY300};
+background-color: ${GRAY_SCALE.GRAY100};
+padding: 10px;
+font-size: 15px;
+color: ${TEXT.GRAY};
+`;
+
+export default VideoDescription;
