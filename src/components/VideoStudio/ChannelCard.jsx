192269f315962a0f0089ed1511d6a94465109b5f
An Hee Jae <gmlwo000810@gmail.com>
2025-04-07 13:58:58 +0900

    버튼 조건부 비활성화 처리 및 실제 데이터 연동

--- src/components/VideoStudio/ChannelCard.jsx
+++ src/components/VideoStudio/ChannelCard.jsx
@@ -1,55 +1,73 @@
-import { Avatar } from "antd";
+import Avatar from "../../components/User/Avatar.jsx";
 import styled from "styled-components";
 import Button1 from "../Button/Button1";
-import { GRAY_SCALE, TEXT } from "../../constants/colors";
+import { GRAY_SCALE, TEXT, WARNING } from "../../constants/colors";
 import FONT from "../../constants/fonts.js";
+import { Alert, Tooltip } from "antd";
 
-function ChannelCard({name, account, onClick}) {
+function ChannelCard({ name, account, onClick, profileUrl, wallet }) {
   return (
     <>
-    <ChannelCardWrapper>
-      <Avatar size={100}/>
-      <InfoWrapper>
-        <ChannelName>{name}</ChannelName>
-        <ChannelAccount>{account}</ChannelAccount>
-      </InfoWrapper>
-      <ButtonWrapper>
-      <Button1
-      onClick={onClick} 
-      width="150px" height="40px" fontSize="18px">업로드</Button1>
-      </ButtonWrapper>
-    </ChannelCardWrapper>
+      <ChannelCardWrapper>
+        <Avatar size={85} src={profileUrl} />
+        <InfoWrapper>
+          <ChannelName>{name}</ChannelName>
+          <ChannelAccount>{account}</ChannelAccount>
+        </InfoWrapper>
+        <ButtonWrapper>
+          <Button1
+            onClick={onClick}
+            width="150px"
+            height="40px"
+            fontSize="18px"
+            disabled={!wallet}
+          >
+            업로드
+          </Button1>
+          {wallet ? null : <AlertMessage>지갑을 연결하세요!</AlertMessage>}
+        </ButtonWrapper>
+      </ChannelCardWrapper>
     </>
-  )
+  );
 }
 
 const ChannelCardWrapper = styled.div`
-display: flex;
-gap:20px;
-padding: 20px 10%;
-`
+  display: flex;
+  gap: 20px;
+  padding: 20px 10%;
+`;
 
 const InfoWrapper = styled.div`
-flex-grow: 1;
-display: flex;
-flex-direction: column;
-justify-content: center;
-gap:5px;
-`
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 5px;
+`;
 
 const ChannelName = styled.div`
-${FONT.TITLE};
-${TEXT.BLACK};
-`
+  ${FONT.TITLE};
+  ${TEXT.BLACK};
+`;
 
 const ChannelAccount = styled.div`
-${FONT.BODY2};
-color: ${GRAY_SCALE.GRAY700};
-`
+  ${FONT.BODY2};
+  color: ${GRAY_SCALE.GRAY700};
+`;
 
 const ButtonWrapper = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+gap: 3px;
+`;
+
+const AlertMessage = styled.div`
   display: flex;
-  align-items: center;
+  justify-content: center;
+  font-size: 13px;
+  color: ${GRAY_SCALE.GRAY700};
 `
 
 export default ChannelCard;
