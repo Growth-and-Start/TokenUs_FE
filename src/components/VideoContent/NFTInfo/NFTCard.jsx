5121fc30b77a2c6f97c3196a96ffe982ad05fa5c
An Hee Jae <gmlwo000810@gmail.com>
2025-05-02 12:42:00 +0900

    썸네일 이미지 오류 시 디폴트 썸네일 대체 처리

--- src/components/VideoContent/NFTInfo/NFTCard.jsx
+++ src/components/VideoContent/NFTInfo/NFTCard.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import thumbnail from "../../../assets/default-thumbnail.png";
+import defaultThumbnail from "../../../assets/default-thumbnail.png";
 import { GRAY_SCALE, MAIN, TEXT } from "../../../constants/colors";
 import { weiToMatic } from "../../../utils/blockchainNetwork";
 import MaticIcon from "../../../assets/matic.png";
@@ -7,40 +7,50 @@ import { useEffect, useState } from "react";
 import { getUserDetail } from "../../../services/channelService";
 import { getVideoDetail } from "../../../services/videoService";
 
-function NFTCard({ className, title, videoId, creatorId, price}) {
-  const [creator, setCreator] = useState('');  //크리에이터 이름(채널명)
-  const [imgURL, setImgURL] = useState('');   //썸네일 이미지
-  
+function NFTCard({ className, title, videoId, creatorId, price }) {
+  const [creator, setCreator] = useState(""); //크리에이터 이름(채널명)
+  const [imgURL, setImgURL] = useState(""); //썸네일 이미지
+
   //matic 단위 변환
-  const priceToMatic = (Number(weiToMatic(price))).toFixed(3);
+  const priceToMatic = Number(weiToMatic(price)).toFixed(3);
 
   //썸네일 이미지 & 크리에이터 이름 가져오기
-  useEffect(()=>{
-    const fetchData = async() => {
-      try{
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
         const videoData = await getVideoDetail(videoId);
         setImgURL(videoData.thumbnailUrl);
         const userData = await getUserDetail(creatorId);
         setCreator(userData.nickName);
-      }catch(error){
-        console.log("크리에이터 이름 가져오기 실패", error)
+      } catch (error) {
+        console.log("크리에이터 이름 가져오기 실패", error);
       }
-    }
+    };
 
     fetchData();
-  },[]);
+  }, []);
 
   return (
     <CardWrapper className={className}>
       <ContentSection>
-        <Thumbnail src={imgURL ? imgURL : thumbnail} alt="thumbnail" />
+        <Thumbnail
+          src={imgURL || defaultThumbnail}
+          onError={(e) => {
+            e.target.onerror = null; 
+            e.target.src = defaultThumbnail;
+          }}
+          alt="thumbnail"
+        />
         <TextSection>
           <Title>{title}</Title>
           <Creator>{creator}</Creator>
         </TextSection>
       </ContentSection>
       <BottomSection>
-        <Price><PriceIcon src={MaticIcon}/>{priceToMatic} MATIC</Price>
+        <Price>
+          <PriceIcon src={MaticIcon} />
+          {priceToMatic} MATIC
+        </Price>
         <InterestButton>+ 관심 등록</InterestButton>
       </BottomSection>
     </CardWrapper>
@@ -65,9 +75,9 @@ const CardWrapper = styled.div`
 `;
 
 const ContentSection = styled.div`
-box-sizing: border-box;
-padding: 7px;
-`
+  box-sizing: border-box;
+  padding: 7px;
+`;
 
 const Thumbnail = styled.img`
   box-sizing: border-box;
@@ -102,17 +112,15 @@ const Creator = styled.div`
 `;
 
 const BottomSection = styled.div`
-height: 25px;
+  height: 25px;
   padding: 12px 16px;
-  background: linear-gradient(45deg, #6C6C6C, #343434);
+  background: linear-gradient(45deg, #6c6c6c, #343434);
   color: white;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  gap:10px;
+  gap: 10px;
 `;
-
-
 
 const Price = styled.div`
   display: flex;
@@ -120,26 +128,25 @@ const Price = styled.div`
   font-weight: 400;
   color: ${MAIN.GREEN};
   font-size: 14px;
-  white-space: nowrap;         // 한 줄로 유지
-  overflow: hidden;            // 넘친 부분 숨김
-  text-overflow: ellipsis;     // 말줄임표 적용
+  white-space: nowrap; // 한 줄로 유지
+  overflow: hidden; // 넘친 부분 숨김
+  text-overflow: ellipsis; // 말줄임표 적용
 `;
 
 const PriceIcon = styled.img`
-width: 15px;
-height: 15px;
-margin-right: 5px;
-object-fit: contain;
+  width: 15px;
+  height: 15px;
+  margin-right: 5px;
+  object-fit: contain;
 `;
 
 const InterestButton = styled.div`
-flex-shrink: 0;
-white-space: nowrap;
+  flex-shrink: 0;
+  white-space: nowrap;
   font-size: 13px;
   /* text-decoration: underline; */
-  &:hover{
+  &:hover {
     cursor: pointer;
     color: ${MAIN.GREEN};
   }
-  
 `;
