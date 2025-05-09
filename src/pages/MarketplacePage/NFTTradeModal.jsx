c56222bb42b69d26fd53e3697df635d699ad98cf
An Hee Jae <gmlwo000810@gmail.com>
2025-05-09 16:16:37 +0900

    NFT 구매 모달창

---
+++ src/pages/MarketplacePage/NFTTradeModal.jsx
@@ -0,0 +1,154 @@
+import styled from "styled-components";
+import BasicModalLayout from "../../components/Modal/Layout/BasicModalLayout";
+import QuantityButton from "../../components/Input/QuantityButton";
+import Button1 from "../../components/Button/Button1";
+import { useState } from "react";
+import { GRAY_SCALE, MAIN } from "../../constants/colors";
+import { weiToMatic } from "../../utils/blockchainNetwork";
+import { transferVideoNFT } from "../../services/NFTService";
+
+function NFTTradeModal({ onClose, listedNFT }) {
+  const [count, setCount] = useState(0);
+
+  //선택한 NFT 구매하기
+  const submitTrade = async(NFTs) => {
+    NFTs.map(async(nft)=>{
+      await transferVideoNFT(nft.tokenId)
+    })
+  }
+
+  return (
+    <>
+      <BasicModalLayout
+        width="32%"
+        onClose={onClose}
+        header={"NFT 구매하기"}
+        footer={
+          <SubmitButton>
+            <Button1 onClick={submitTrade(listedNFT.slice(0,count))} width="150px" height="40px" fontSize="18px">
+              구매하기
+            </Button1>
+          </SubmitButton>
+        }
+      >
+        <ModalBody>
+          <AmountBox>
+            <Title>구매 수량</Title>
+            <QuantityButton
+              count={count}
+              setCount={setCount}
+              upperBound={listedNFT.length}
+            />
+          </AmountBox>
+          <NFTBox>
+            <Title>구매 NFT</Title>
+            <NFTList>
+              {listedNFT.slice(0, count).map((nft, index) => (
+                <SelectedNFT
+                  key={index}
+                  name={nft.nftName}
+                  id={nft.tokenId}
+                  price={nft.currentPrice}
+                />
+              ))}
+            </NFTList>
+          </NFTBox>
+          <TotalBox>
+            <TotalTitle>총 가격</TotalTitle>
+            <TotalPrice>
+              {listedNFT
+                .slice(0, count)
+                .reduce(
+                  (acc, nft) => acc + Number(weiToMatic(nft.currentPrice)),
+                  0
+                )
+                .toFixed(3)}{" "}
+              MATIC
+            </TotalPrice>
+          </TotalBox>
+        </ModalBody>
+      </BasicModalLayout>
+    </>
+  );
+}
+
+const ModalBody = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const Title = styled.div`
+  font-weight: 520;
+  font-size: 15px;
+`;
+
+const AmountBox = styled.div`
+  display: flex;
+  gap: 20px;
+`;
+
+const NFTBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+const NFTList = styled.div``;
+
+const TotalBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 30px;
+  padding: 10px 0;
+  border-top: 1px solid ${GRAY_SCALE.GRAY300};
+`;
+
+const TotalTitle = styled.div`
+  font-weight: 520;
+  font-size: 18px;
+`;
+
+const TotalPrice = styled.div`
+  font-weight: 520;
+  font-size: 18px;
+  color: ${MAIN.BLUE};
+`;
+
+const SubmitButton = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+function SelectedNFT({ name, id, price }) {
+  price = Number(weiToMatic(price)).toFixed(3);
+  return (
+    <BoxWrapper>
+      <NFTName>{name}</NFTName>
+      <TokenId>({id})</TokenId>
+      <NFTPrice>{price}&nbsp;&nbsp;MATIC</NFTPrice>
+    </BoxWrapper>
+  );
+}
+
+const BoxWrapper = styled.div`
+  display: flex;
+  gap: 5px;
+  border-radius: 5px;
+  padding: 5px 10px;
+  border: 1px solid ${GRAY_SCALE.GRAY500};
+  background-color: white;
+`;
+const NFTName = styled.div``;
+
+const TokenId = styled.div``;
+
+const NFTPrice = styled.div`
+  flex-grow: 1;
+  display: flex;
+  justify-content: end;
+  color: ${MAIN.BLUE};
+`;
+
+export default NFTTradeModal;
