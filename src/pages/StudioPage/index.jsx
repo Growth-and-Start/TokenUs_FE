514bea1066ec154fcc8560253ebec50a28a75af3
An Hee Jae <gmlwo000810@gmail.com>
2025-05-09 15:55:08 +0900

    더미 데이터 삭제

--- src/pages/StudioPage/index.jsx
+++ src/pages/StudioPage/index.jsx
@@ -6,46 +6,6 @@ import UploadModalController from "./UploadModalController.jsx";
 import { getMyInfo } from "../../services/channelService.js";
 import { getMyVideo } from "../../services/videoService.js";
 
-const tempData_channel = {
-  name: "8시 스쿼시 연맹",
-  account: "tokenus.creator@gmail.com",
-};
-
-const tempData_videoTable = [
-  {
-    id: 1,
-    title: "React로 만드는 멋진 웹앱",
-    summary: "React 프레임워크를 활용해 나만의 웹앱 만들기 튜토리얼",
-    isPublic: true,
-    uploadDate: "2024.02.24",
-    NFTPrice: "0.00027ETH",
-  },
-  {
-    id: 2,
-    title: "React로 만드는 멋진 웹앱",
-    summary: "React 프레임워크를 활용해 나만의 웹앱 만들기 튜토리얼",
-    isPublic: false,
-    uploadDate: "2024.02.24",
-    NFTPrice: "0.00027ETH",
-  },
-  {
-    id: 3,
-    title: "React로 만드는 멋진 웹앱",
-    summary: "React 프레임워크를 활용해 나만의 웹앱 만들기 튜토리얼",
-    isPublic: true,
-    uploadDate: "2024.02.24",
-    NFTPrice: "0.00027ETH",
-  },
-  {
-    id: 4,
-    title: "React로 만드는 멋진 웹앱",
-    summary: "React 프레임워크를 활용해 나만의 웹앱 만들기 튜토리얼",
-    isPublic: false,
-    uploadDate: "2024.02.24",
-    NFTPrice: "0.00027ETH",
-  },
-];
-
 function StudioPage() {
   //로딩 상태
   const [loading, setLoading] = useState(false);
