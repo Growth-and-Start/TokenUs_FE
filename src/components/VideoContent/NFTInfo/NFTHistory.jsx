094cb47d55baf810dc793caa8af6a37001519c39
An Hee Jae <gmlwo000810@gmail.com>
2025-05-09 15:50:08 +0900

    거래 내역 없을 시 UI 추가 및 더미 데이터 삭제

--- src/components/VideoContent/NFTInfo/NFTHistory.jsx
+++ src/components/VideoContent/NFTInfo/NFTHistory.jsx
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 import { MAIN, GRAY_SCALE, BACKGROUND } from "../../../constants/colors";
 
-
 function Holder({ account, price, index }) {
   return (
     <>
@@ -33,23 +32,79 @@ const NFTPrice = styled.div`
   font-size: 15px;
 `;
 
-function NFTHistory({history, className}) {
+// const tempData = {
+//   NFTPrice: 2.71,
+//   NFThistory: [
+//     {
+//       account: "0xa1b2...c3d4",
+//       price: "2.70",
+//     },
+//     {
+//       account: "0xbeef...feed",
+//       price: "2.72",
+//     },
+//     {
+//       account: "0xdeed...face",
+//       price: "2.65",
+//     },
+//     {
+//       account: "0xfade...d00d",
+//       price: "2.68",
+//     },
+//     {
+//       account: "0xdead...beef",
+//       price: "2.54",
+//     },
+//     {
+//       account: "0xbaad...cafe",
+//       price: "2.68",
+//     },
+//     {
+//       account: "0x0ff1...ce00",
+//       price: "2.42",
+//     },
+//     {
+//       account: "0xc001...d00d",
+//       price: "2.56",
+//     },
+//     {
+//       account: "0xc002...d30d",
+//       price: "2.53",
+//     },
+//     {
+//       account: "0xc001...d00d",
+//       price: "2.50",
+//     },
+//     {
+//       account: "0xc001...d00d",
+//       price: "2.43",
+//     },
+//     {
+//       account: "0xc001...d00d",
+//       price: "2.56",
+//     },
+//   ],
+// };
+
+function NFTHistory({ history, className }) {
   return (
     <>
       <HolderListWrapper className={className}>
-        <Title>
-          Transaction History
-        </Title>
-        <List>
-          {history.map((holder, index) => (
-            <Holder
-              key={index}
-              account={holder.account}
-              price={holder.price}
-              index={index}
-            />
-          ))}
-        </List>
+        <Title>Transaction History</Title>
+        {history && history.length > 0 ? (
+          <List>
+            {history.map((holder, index) => (
+              <Holder
+                key={index}
+                account={holder.account}
+                price={holder.price}
+                index={index}
+              />
+            ))}
+          </List>
+        ) : (
+          <NoTxMessage>거래 내역이 없습니다.</NoTxMessage>
+        )}
       </HolderListWrapper>
     </>
   );
@@ -70,19 +125,26 @@ const Title = styled.div`
 `;
 
 const List = styled.div`
-  max-height: 100%; 
-  overflow-y: auto; 
-  scrollbar-width: thin; 
-  scrollbar-color: ${GRAY_SCALE.GRAY300} transparent; 
+  max-height: 100%;
+  overflow-y: auto;
+  scrollbar-width: thin;
+  scrollbar-color: ${GRAY_SCALE.GRAY300} transparent;
 
   &::-webkit-scrollbar {
-    width: 3px; 
+    width: 3px;
   }
 
   &::-webkit-scrollbar-thumb {
-    background-color: ${GRAY_SCALE.GRAY300}; 
+    background-color: ${GRAY_SCALE.GRAY300};
     border-radius: 4px;
   }
+`;
+
+const NoTxMessage = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 30% 0;
+  background-color: ${GRAY_SCALE.GRAY100};
 `;
 
 export default NFTHistory;
