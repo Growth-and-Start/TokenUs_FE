f2f0352bcb6d113335086ae608d2532b6c8d8fd1
An Hee Jae <gmlwo000810@gmail.com>
2025-05-17 16:17:37 +0900

    메인 페이지 인기 영상 NFT 정보 연동 및 구성 변경

--- src/components/VideoContent/Main/NFTInfo.jsx
+++ src/components/VideoContent/Main/NFTInfo.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { MAIN, GRAY_SCALE, TEXT } from "../../../constants/colors";
+import { MAIN, GRAY_SCALE, TEXT, BACKGROUND } from "../../../constants/colors";
 import QuantityButton from "../../Input/QuantityButton";
 import { useEffect, useState } from "react";
 import Button1 from "../../Button/Button1";
@@ -8,40 +8,47 @@ import FONT from "../../../constants/fonts";
 import { weiToMatic } from "../../../utils/blockchainNetwork";
 import { getUserDetail } from "../../../services/channelService";
 
-function NFTInfo({video}) {
+function NFTInfo({ video }) {
   const [count, setCount] = useState(1);
   const [creator, setCreator] = useState("");
-  const TotalPrice = 5.07;
 
-  const getCreatorName = async() => {
+  const getCreatorName = async () => {
     const data = await getUserDetail(video.creatorId);
-    return data.nickName;
-  }
+    setCreator(data.nickName);
+  };
 
-  useEffect(()=>{
-    setCreator(getCreatorName());
-  },[])
+  useEffect(() => {
+    getCreatorName();
+  }, []);
   return (
     <>
       <NFTInfoWrapper>
         <BasicInfo>
-          <Header>Mint Price</Header>
+          <VideoTitle>{video.videoTitle}</VideoTitle>
+          <Creator>{creator}</Creator>
+          {/* <Header>Mint Price</Header>
           <Header>Floor Price</Header>
           <Header>Creator</Header>
           <Data>{weiToMatic(video.mintPrice)}</Data>
           <Data>{weiToMatic(video.floorPrice)}</Data>
-          <Data>{creator}</Data>
+          <Data>{creator}</Data> */}
         </BasicInfo>
         <PriceInfo>
-          <Title>예상 가격</Title>
+          <Title>Floor Price</Title>
           <Price>
-            <CurrentPrice>{TotalPrice * count} MATIC</CurrentPrice>
-            <QuantityButton count={count} setCount={setCount}/>
+            <CurrentPrice>
+              {weiToMatic(video.floorPrice) || 0} MATIC
+            </CurrentPrice>
+            {/* <QuantityButton count={count} setCount={setCount}/> */}
           </Price>
         </PriceInfo>
         <Buttons>
-          <Button2 width={'150px'} fontSize={'18px'}>NFT 보러가기</Button2>
-          <Button1 width={'150px'} fontSize={'18px'}>구매하기</Button1>
+          <Button2 width={"150px"} fontSize={"18px"}>
+            NFT 보러가기
+          </Button2>
+          <Button1 width={"150px"} fontSize={"18px"}>
+            구매하기
+          </Button1>
         </Buttons>
       </NFTInfoWrapper>
     </>
@@ -49,21 +56,57 @@ function NFTInfo({video}) {
 }
 
 const NFTInfoWrapper = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: space-between;
   width: 35%;
   border: 1px solid ${GRAY_SCALE.GRAY300};
   border-radius: 5px;
   padding: 10px 15px;
 `;
 const BasicInfo = styled.div`
-  display: grid;
-  grid-template-columns: 1.5fr 1.5fr 2.5fr;
+  /* display: grid;
+  grid-template-columns: 1.5fr 1.5fr 2.5fr; */
+  display: flex;
+  flex-direction: column;
   gap: 10px;
   border-bottom: 1px solid ${GRAY_SCALE.GRAY300};
   padding-bottom: 15px;
-  margin-bottom: 20px;
+
 `;
+
+const VideoTitle = styled.div.attrs((props) => ({
+  title: props.children,
+}))`
+  color: ${TEXT.BLACK};
+  font-size: 20px;
+
+  display: -webkit-box;
+  -webkit-line-clamp: 2; // 최대 2줄
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+
+  line-height: 1.4; // 줄 간격
+  max-height: calc(1.4em * 2); // 두 줄 기준 높이 고정
+`;
+
+const Creator = styled.div.attrs((props) => ({
+  title: props.children,
+}))`
+  color: ${TEXT.BLACK};
+  font-size: 14px;
+
+  display: -webkit-box;
+  -webkit-line-clamp: 1; // 최대 2줄
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+
+  line-height: 1.4; // 줄 간격
+  max-height: calc(1.4em * 2); // 두 줄 기준 높이 고정
+`;
+
 const Header = styled.div`
-font-size: 15px;
+  font-size: 15px;
 `;
 
 const Data = styled.div`
@@ -73,30 +116,31 @@ const Data = styled.div`
 `;
 
 const PriceInfo = styled.div`
-padding: 0 10px;
-margin-bottom: 30px;
+  padding: 0 10px;
 `;
 
 const Title = styled.div`
-color: ${GRAY_SCALE.GRAY700};
-${FONT.CAPTION}
-font-size: 13px;
+  color: ${GRAY_SCALE.GRAY700};
+  ${FONT.CAPTION}
+  font-size: 13px;
 `;
 
 const Price = styled.div`
-display: flex;
-justify-content: space-between;
+  display: flex;
+  justify-content: space-between;
 `;
 
 const CurrentPrice = styled.div`
-${FONT.TITLE};
-color : ${TEXT.BLACK};
+  font-size: 24px;
+  font-weight: 600;
+  color: ${TEXT.BLACK};
 `;
 
 const Buttons = styled.div`
   display: flex;
   justify-content: center;
-  gap:20px;
-`
+  gap: 20px;
+  margin-bottom: 10px;
+`;
 
 export default NFTInfo;
